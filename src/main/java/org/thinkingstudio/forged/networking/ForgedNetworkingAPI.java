5557336f1d81a3712428c295d25d844488e59e6c
TexTrue <65154269+TexBlock@users.noreply.github.com>
2024-02-17 14:01:45 +0800

    clean

--- src/main/java/org/thinkingstudio/forged/networking/ForgedNetworkingAPI.java
+++ src/main/java/org/thinkingstudio/forged/networking/ForgedNetworkingAPI.java
@@ -2,6 +2,11 @@ package org.thinkingstudio.forged.networking;
 
 import net.fabricmc.fabric.impl.networking.CommonPacketsImpl;
 import net.fabricmc.fabric.impl.networking.client.ClientNetworkingImpl;
+
+import net.minecraft.SharedConstants;
+import net.minecraft.server.command.DebugConfigCommand;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.fml.common.Mod;
 
 import net.minecraftforge.fml.loading.FMLLoader;
@@ -15,10 +20,28 @@ public class ForgedNetworkingAPI {
     public static final Logger LOGGER = LoggerFactory.getLogger(MODNAME);
 
     public ForgedNetworkingAPI() {
+        CommonPacketsImpl.init();
         if (FMLLoader.getDist().isClient()) {
             ClientNetworkingImpl.clientInit();
         }
-        CommonPacketsImpl.init();
+
+        if (isDevelopmentEnvironment()) {
+            MinecraftForge.EVENT_BUS.addListener(this::registerCommand);
+        }
+    }
+
+    public void registerCommand(RegisterCommandsEvent event) {
+        if (SharedConstants.isDevelopment) {
+            // Command is registered when isDevelopment is set.
+            return;
+        }
+
+        if (!ForgedNetworkingAPI.isDevelopmentEnvironment()) {
+            // Only register this command in a dev env
+            return;
+        }
+
+        DebugConfigCommand.register(event.getDispatcher());
     }
 
     public static boolean isDevelopmentEnvironment() {
